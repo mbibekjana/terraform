0f35e12fdc9b05384edb15023bba7d7ad4665597
bibek <bibek@mydreamholiday.in>
2022-05-29 11:56:00 +0530

    working

--- vpc.tf
+++ vpc.tf
@@ -36,3 +36,49 @@ resource "aws_subnet" "private2" {
     Name = "private2"
   }
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_eip" "eip" {
+  vpc      = true
+}
+resource "aws_nat_gateway" "natg" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public1.id
+
+}
+resource "aws_route_table" "IG_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  
+}
+resource "aws_route_table" "NAT_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natg.id
+  }
+
+}
+
+resource "aws_route_table_association" "public_subnet1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.IG_route_table.id
+}
+resource "aws_route_table_association" "public_subnet2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.IG_route_table.id
+}
+resource "aws_route_table_association" "private_subnet1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.NAT_route_table.id
+}
+resource "aws_route_table_association" "private_subnet2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.NAT_route_table.id
+}
