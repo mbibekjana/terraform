d83935217a0b636b5f6c9a6d7909c2add2a04feb
bibek <bibek@mydreamholiday.in>
2022-05-29 10:28:57 +0530

    working

--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,35 @@ resource "aws_vpc" "main" {
     Name = "terraform"
   }
 }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Main"
+    Name = "public1"
+  }
+}
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public2"
+  }
+}
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.11.0/24"
+
+  tags = {
+    Name = "private1"
+  }
+}
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.12.0/24"
+
+  tags = {
+    Name = "private2"
   }
 }
